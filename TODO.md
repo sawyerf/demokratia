1ded27f32c7ec020bbc7b38fc7d622c9bb8ce153
Sawyerf <sawyer.flink@protonmail.ch>
2019-10-06 03:10:45 +0200

    Fix: voteglobal && Add: TODO

---
+++ TODO.md
@@ -0,0 +1,12 @@
+# TODO
+1. INBOX
+	- debug
+2. OUTBOX
+	- updated: Time.Now
+3. Add comment
+4. Add Filter
+5. Add token with limit of time
+6. Add list of invit
+7. Add contact btw 2 site
+8. Add Real form for vote
+9. Add Forum
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -46,37 +46,52 @@ class VotesController < ApplicationController
     vote.vote(nvote, @current_user.voter_hash)
   end
 
-  def voteglobal(vote)
-    nvote = params[:vote].to_i
-    votelog = VoteLog.where(voter_hash: @current_user.voter_hash, vote_id: params[:id]).first
-    if vote.choice_count <= nvote or nvote < 0
-      return 
-    elsif votelog and votelog.vote == nvote
-      nvote = -1
-    end
-    vote.vote(nvote, @current_user.voter_hash)
-    votelog = VoteLog.where(voter_hash: @current_user.voter_hash, vote_id: params[:id]).first
-    site_key = Site.find(vote.site_id).mykey
+  def sendvote_inbox(site_id, votelog, nvote)
+    site_key = Site.find(site_id).mykey
     json = votelog.json_inbox(nvote, site_key)
     http = Net::HTTP.new("localhost", 3001)
     http.use_ssl = false
     request = Net::HTTP::Post.new("/inbox", {'Content-Type' => 'application/json'})
     request.body = json
-    response = http.request(request)
-    items = JSON.parse(response.body)
-    flash[:success] = items["items"]
-    items["items"].each do |item|
-      if item["type"] == "vote"
-        choices = Choice.where(vote_id: vote.id)
-        vote.update voter_count: item["voter_count"], status: item["status"], winner: item["winner"]
-        i = 0
-        item["choices"].each do |vote_count|
-          choices[i].update vote_count: vote_count
-          i = i + 1
+    begin
+      return http.request(request)
+    rescue => e
+      flash[:fail] = e.message
+      return nil
+    end
+  end
+
+  def voteglobal(vote)
+    nvote = params[:vote].to_i
+    votelog = VoteLog.where(voter_hash: @current_user.voter_hash, vote_id: params[:id]).first
+    if vote.choice_count <= nvote or nvote < 0
+      return 
+    elsif not votelog
+      votelog = VoteLog.create vote_id: vote.id, vote: -1, site_id: vote.site_id, voter_hash: @current_user.voter_hash
+    elsif votelog.vote == nvote
+      nvote = -1
+    end
+    response = sendvote_inbox(vote.site_id, votelog, nvote)
+    if not response
+      return 
+    elsif response.code == "200"
+      items = JSON.parse(response.body)
+      flash[:success] = items["items"]
+      items["items"].each do |item|
+        if item["type"] == "vote"
+          choices = Choice.where(vote_id: vote.id)
+          vote.update voter_count: item["voter_count"], status: item["status"], winner: item["winner"]
+          i = 0
+          item["choices"].each do |vote_count|
+            choices[i].update vote_count: vote_count
+            i = i + 1
+          end
+        elsif item["type"] == "votelog"
+          votelog.update vote: item["vote"]
         end
-      elsif item["type"] == "votelog"
-        votelog.update vote: item["vote"]
       end
+    else
+      flash[:fail] = "Fail code: `" + response.code + "`"
     end
   end
 
